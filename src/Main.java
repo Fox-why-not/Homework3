e5c7d976dfbe31c497e772272a43fd5e202df554
Fox-Why-Not <Clover.kos.1@gmail.com>
2024-04-15 21:47:49 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+    int I = 1111111111;
+    byte B = 127;
+    short S = 32767;
+    long L = 9223372036854775807L;
+    float F = 1.1111111f;
+    double D = 1.1111111111111111;
+    System.out.println("значение переменной " + "I " + "с типом " + "int " + "равно " + I );
+    System.out.println("значение переменной " + "B " + "с типом " + "byte " + "равно " + B );
+    System.out.println("значение переменной " + "S " + "с типом " + "short " + "равно " + S );
+    System.out.println("значение переменной " + "L " + "с типом " + "long " + "равно " + L );
+    System.out.println("значение переменной " + "F " + "с типом " + "float " + "равно " + F );
+    System.out.println("значение переменной " + "D " + "с типом " + "doudle " + "равно " + D );
+    float F2 = 27.12f;
+    long L2 = 987678965549L;
+    float F3 = 2.786f;
+    short S2 = 569;
+    short S4 = -159;
+    short S5 = 27897;
+    byte B2 = 67;
+    byte LP = 23;
+    byte AS = 27;
+    byte EA = 30;
+    short TotalP = 480;
+    int ForEveryone = TotalP / (LP+AS+EA);
+    System.out.println("На каждого ученика рассчитано " + ForEveryone + " листов бумаги");
+
     }
 }
