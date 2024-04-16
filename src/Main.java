d402b661ed4ed4f8d90baa896292a25167fdba81
Fox-Why-Not <Clover.kos.1@gmail.com>
2024-04-16 19:21:30 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -25,6 +25,49 @@ public class Main {
     short TotalP = 480;
     int ForEveryone = TotalP / (LP+AS+EA);
     System.out.println("На каждого ученика рассчитано " + ForEveryone + " листов бумаги");
-
+        byte BottlesInTwoMinutes =16;
+        int BottlesPerMinute =BottlesInTwoMinutes / 2;
+        byte Hour = 60;
+        int Day = Hour * 24;
+        int Month = Day * 30;
+        int BottlesIn20Minutes = BottlesPerMinute * 20;
+        int BottelsPerDay =BottlesPerMinute * Day;
+        int BottelsInThreeDays =BottlesPerMinute * (Day * 3);
+        int BottelsPerMonth =BottlesPerMinute * Month;
+        System.out.println ("за двадцать минут машина произвела " + BottlesIn20Minutes + "штук бутылок");
+        System.out.println ("за день машина произвела " + BottelsPerDay + " штук бутылок");
+        System.out.println ("за три дня машина произвела " + BottelsInThreeDays + " штук бутылок");
+        System.out.println ("за месяц машина произвела " + BottelsPerMonth + " штук бутылок");
+        byte WhiteForClass = 2;
+        byte BrownForClass = 4;
+        int PaintsForClass= BrownForClass + WhiteForClass;
+        byte Cans = 120;
+        int Classes = Cans / PaintsForClass;
+        int TotalW = Classes * WhiteForClass;
+        int TotalB = Classes * BrownForClass;
+        System.out.println ("В школе, где " + Classes + " классов, нужно " + TotalW + " банок белой краски и " + TotalB + " банок коричневой краски. ") ;
+        int SalaryM = 67760;
+        int SalaryD = 83690;
+        int SalaryK = 76230;
+        double Bonus = 1.1;
+        byte Year55 = 12;
+        double BonSalMInMonth = SalaryM * Bonus;
+        double BonSalDInMonth = SalaryD * Bonus;
+        double BonSalKInMonth = SalaryK * Bonus;
+        int SalMInYear = SalaryM * Year55;
+        int SalDInYear = SalaryD * Year55;
+        int SalKInYear = SalaryK * Year55;
+        double BonSalMInYear = BonSalMInMonth * Year55;
+        double BonSalDInYear = BonSalDInMonth * Year55;
+        double BonSalKInYear = BonSalKInMonth * Year55;
+        double DifferenceSalM = BonSalMInYear - SalMInYear;
+        double DifferenceSalD = BonSalDInYear - SalDInYear;
+        double DifferenceSalK = BonSalKInYear - SalKInYear;
+        System.out.println (" Маша теперь получает " + BonSalMInMonth + " рублей." +
+                " Годовой доход вырос на " + DifferenceSalM + " рублей."
+                + " Денис теперь получает " + BonSalDInMonth + " рублей." +
+                " Годовой доход вырос на " + DifferenceSalD + " рублей. "
+                + " Кристина теперь получает " + BonSalKInMonth + " рублей." +
+                " Годовой доход вырос на " + DifferenceSalK + " рублей");
     }
 }
